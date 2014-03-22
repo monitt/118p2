c127d14e778b477cfc2d37b4e3ffe5cc992ad9df
Monit Tyagi <lakers1230@gmail.com>
2014-03-21 18:01:35 -0700

    t24
    omfg

--- router/sr_router.c
+++ router/sr_router.c
@@ -329,9 +329,9 @@ void sr_handlepacket(struct sr_instance* sr,
 
  printf("*** 111111");
   struct sr_if* iface = sr_get_interface(sr, interface);
-  if(ethertype(packet) == ethertype_arp) 
-     handleArpPacket(sr, (sr_arp_hdr_t* )(packet+sizeof(sr_ethernet_hdr_t)), len-sizeof(sr_ethernet_hdr_t), iface);
- printf("*** 22222");
+  if(ethertype(packet) == ethertype_arp) {
+     handleArpPacket(sr, (sr_arp_hdr_t* )(packet+sizeof(sr_ethernet_hdr_t)), len-sizeof(sr_ethernet_hdr_t), iface); printf("*** 22222");
+ }
   else if(ethertype(packet) == ethertype_ip)
   {
     int i=0;
